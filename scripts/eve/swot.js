e8535c17a4083d91cc711652e6268da4b43ed350
assessdocs <sistemaautodocs+githubnova@gmail.com>
2026-04-07 15:00:45 -0300

    Update swot.js

--- scripts/eve/swot.js
+++ scripts/eve/swot.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var swotSwitcher = document.getElementById('swot-switch')
     var tituloDocumento = document.getElementById('titulo-documento')
 
-    tituloDocumento.innerHTML = 'ESTUDO DE<br>VIABILIDADE<br>ECONÔMICA';
+    tituloDocumento.innerHTML = 'ESTUDO<br>TÉCNICO DE<br>VIABILIDADE<br>ECONÔMICA';
 
     swotSwitcher.addEventListener('change', function () {
         if (this.checked) {
